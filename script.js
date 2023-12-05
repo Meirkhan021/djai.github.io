3f2522339e481b91137020320cb7baf1e76564b5
Meirkhan021 <152962713+Meirkhan021@users.noreply.github.com>
2023-12-05 14:35:53 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Простая функция для добавления анимации при наведении мыши
+function animateElement(element) {
+    element.style.transition = "color 0.3s ease-in-out";
+    element.style.color = "#e74c3c"; // Измените цвет по вашему усмотрению
+}
